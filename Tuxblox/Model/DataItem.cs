b3b7379066968b4c0ef6043be7af08acfc88c6eb
voodoo123x <timoby86@gmail.com>
2018-09-16 22:28:22 -0400

    Add project files.

---
+++ Tuxblox/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Tuxblox.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
